c476835589a027bc3802eb573ceeba5b87dc10fb
gitdataforge <your-id+username@users.noreply.github.com>
2026-05-20 16:37:33 +0000

    u

--- parbet/src/components/FirebaseQuotaBlocker.jsx
+++ parbet/src/components/FirebaseQuotaBlocker.jsx
@@ -25,16 +25,16 @@ const AmbientLockdownBackground = () => (
     </div>
 );
 
-// Authentic Firebase Logo SVG Paths
+// Authentic Firebase Logo (PNG Based - 2026)
 const AuthenticFirebaseLogo = () => (
     <div className="flex items-center justify-center gap-3 mt-12 select-none">
-        <svg viewBox="0 0 116 118" width="48" height="48" xmlns="http://www.w3.org/2000/svg">
-            <path d="M16.34 32.74L35.25 0l3.86 3.73-22.77 39.4-1.22-2.1L2.14 18.52l4.88-5.06 9.32 19.28z" fill="#FFA000"/>
-            <path d="M109.43 18.66l-18.73 32.4-32.84-56.9c-.83-1.44-2.88-1.44-3.7 0L24.32 45.86 52.87 95.3l57.77-32.17c1.78-1 2.37-3.23 1.25-4.96l-2.46-39.51z" fill="#F57C00"/>
-            <path d="M52.87 95.3L24.32 45.86l-22.18 38.4c-1.12 1.94-.37 4.38 1.57 5.46L52.87 118V95.3z" fill="#FFCA28"/>
-            <path d="M52.87 118L112.2 84.9c1.94-1.08 2.69-3.52 1.57-5.46L56.57 0c-.83-1.44-2.88-1.44-3.7 0L0 91.24l52.87 26.76z" fill="#FFC107"/>
-            <path d="M52.87 118v-22.7L24.32 45.86 0 88.08l52.87 29.92z" fill="#FFA000"/>
-        </svg>
+        <img 
+            src="firebase-logo.png" 
+            alt="Firebase" 
+            width="48" 
+            height="48" 
+            className="w-12 h-12 object-contain"
+        />
         <svg viewBox="0 0 200 48" width="140" height="34" xmlns="http://www.w3.org/2000/svg">
             <text x="0" y="36" fontFamily="Product Sans, Roboto, sans-serif" fontSize="38" fontWeight="500" fill="#626262" letterSpacing="-0.5">Firebase</text>
         </svg>
